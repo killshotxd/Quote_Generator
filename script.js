e4f6dbc528dd86a933c96807363889249d53589b
killshotxd <hassanansari211@gmail.com>
2022-03-25 11:04:28 +0530

    fetching quote from api

---
+++ script.js
@@ -0,0 +1,17 @@
+let apiQuotes = [];
+
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {}
+}
+
+getQuotes();
